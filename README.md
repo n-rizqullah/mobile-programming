d186e8ac07dd5e201aad1bcfd754177fccadc08c
n-rizqullah <naupalrizqullah83@gmail.com>
2023-04-08 16:38:43 +0700

    upload file tambahan

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# mobile-programming
+# MobilePrograming
+
+Ini adalah repository yang berisi jawaban tugas mobile programming STT Wastukancana 2023
+
+## Create by
+Sonnya Ghandi
---
+++ tugas-2/no2.js
@@ -0,0 +1,11 @@
+// Nama : Sonnya Ghandi
+// Kelas : Informatika Malam A
+// NIM : 201351131
+function kalikan(a, b) {
+    return a * b
+}
+var num1 = 12
+var num2 = 4
+var hasilKali = kalikan(num1, num2)
+console.log(hasilKali) // 48
+console.log();
